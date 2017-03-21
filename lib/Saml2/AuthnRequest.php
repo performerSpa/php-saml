764c3c37fce8adf87a8533ac66dee385e4a840a8
Giuseppe di Bona <giuseppe.dibona@performer.it>
2017-03-21 11:46:26 +0100

    Gestito tramite config.yml la requestedAuthnContext

--- lib/Saml2/AuthnRequest.php
+++ lib/Saml2/AuthnRequest.php
@@ -9,18 +9,21 @@ class OneLogin_Saml2_AuthnRequest
 
     /**
      * Object that represents the setting info
+     *
      * @var OneLogin_Saml2_Settings
      */
     protected $_settings;
 
     /**
      * SAML AuthNRequest string
+     *
      * @var string
      */
     private $_authnRequest;
 
     /**
      * SAML AuthNRequest ID.
+     *
      * @var string
      */
     private $_id;
@@ -28,19 +31,19 @@ class OneLogin_Saml2_AuthnRequest
     /**
      * Constructs the AuthnRequest object.
      *
-     * @param OneLogin_Saml2_Settings $settings Settings
-     * @param bool   $forceAuthn When true the AuthNReuqest will set the ForceAuthn='true'
-     * @param bool   $isPassive  When true the AuthNReuqest will set the Ispassive='true'
+     * @param OneLogin_Saml2_Settings $settings   Settings
+     * @param bool                    $forceAuthn When true the AuthNReuqest will set the ForceAuthn='true'
+     * @param bool                    $isPassive  When true the AuthNReuqest will set the Ispassive='true'
      */
     public function __construct(OneLogin_Saml2_Settings $settings, $forceAuthn = false, $isPassive = false)
     {
         $this->_settings = $settings;
 
-        $spData = $this->_settings->getSPData();
-        $idpData = $this->_settings->getIdPData();
+        $spData   = $this->_settings->getSPData();
+        $idpData  = $this->_settings->getIdPData();
         $security = $this->_settings->getSecurityData();
 
-        $id = OneLogin_Saml2_Utils::generateUniqueID();
+        $id           = OneLogin_Saml2_Utils::generateUniqueID();
         $issueInstant = OneLogin_Saml2_Utils::parseTime2SAML(time());
 
         $nameIDPolicyFormat = $spData['NameIDFormat'];
@@ -48,7 +51,7 @@ class OneLogin_Saml2_AuthnRequest
             $nameIDPolicyFormat = OneLogin_Saml2_Constants::NAMEID_ENCRYPTED;
         }
 
-        $providerNameStr = '';
+        $providerNameStr  = '';
         $organizationData = $settings->getOrganization();
         if (!empty($organizationData)) {
             $langs = array_keys($organizationData);
@@ -96,8 +99,11 @@ ISPASSIVE;
 REQUESTEDAUTHN;
             } else {
                 $requestedAuthnStr .= "    <samlp:RequestedAuthnContext Comparison=\"$authnComparison\">\n";
-                foreach ($security['requestedAuthnContext'] as $contextValue) {
-                    $requestedAuthnStr .= "        <saml:AuthnContextClassRef>".$contextValue."</saml:AuthnContextClassRef>\n";
+
+                $requestedAuthnContextArray = explode(',', $security['requestedAuthnContext']);
+                
+                foreach ($requestedAuthnContextArray as $contextValue) {
+                    $requestedAuthnStr .= "        <saml:AuthnContextClassRef>" . $contextValue . "</saml:AuthnContextClassRef>\n";
                 }
                 $requestedAuthnStr .= '    </samlp:RequestedAuthnContext>';
             }
@@ -122,7 +128,7 @@ REQUESTEDAUTHN;
 </samlp:AuthnRequest>
 AUTHNREQUEST;
 
-        $this->_id = $id;
+        $this->_id           = $id;
         $this->_authnRequest = $request;
     }
 
@@ -133,7 +139,8 @@ AUTHNREQUEST;
     public function getRequest()
     {
         $deflatedRequest = gzdeflate($this->_authnRequest);
-        $base64Request = base64_encode($deflatedRequest);
+        $base64Request   = base64_encode($deflatedRequest);
+
         return $base64Request;
     }
 
